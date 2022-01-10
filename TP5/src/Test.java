f2e9455945a5cd7723b494d76087673761848b24
Swahdev <63730580+Swahdev@users.noreply.github.com>
2022-01-11 00:57:25 +0100

    Delete TP5/src directory

--- TP5/src/Test.java
+++
@@ -1,7 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Formateur test = new Formateur("/Users/charleschikhani/IdeaProjects/TP5/textes.txt");
-        test.read();
-        test.print();
-    }
-}
